8e6028a70a2afff0b51d2c8eb59bc2c09334c5d8
eaves <eavesmy@gmail.com>
2018-06-21 23:26:01 +0800

    Fix dic path

--- src/main.go
+++ src/main.go
@@ -3,12 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"path/filepath"
 )
+
+var PATH string
 
 func main() {
 
+	if len(os.Args) < 2 {
+		PATH = "./"
+	} else {
+		PATH, _ = filepath.Abs(os.Args[1])
+	}
+
 	fmt.Println("File server start at http://127.0.0.1:9990")
-	err := http.ListenAndServe(":9990", http.FileServer(http.Dir("./")))
+	err := http.ListenAndServe(":9990", http.FileServer(http.Dir(PATH)))
 
 	if err != nil {
 		fmt.Println("File server start error: ", err)
