4a78f18ed5fc21f67e7ed48dba1eabedd7afbea4
eaves <eavesmy@gmail.com>
2018-07-17 15:36:04 +0800

    Add reset port func

--- src/main.go
+++ src/main.go
@@ -8,6 +8,7 @@ import (
 )
 
 var PATH string
+var port = 9989
 
 func main() {
 
@@ -17,11 +18,20 @@ func main() {
 		PATH, _ = filepath.Abs(os.Args[1])
 	}
 
-	fmt.Println("File server start at http://127.0.0.1:9990")
-	err := http.ListenAndServe(":9990", http.FileServer(http.Dir(PATH)))
+	run()
+}
+
+func run() error {
+	fmt.Println("File server start at http://127.0.0.1:" + fmt.Sprintf("%d", port))
+	err := http.ListenAndServe(":"+fmt.Sprintf("%d", port), http.FileServer(http.Dir(PATH)))
 
 	if err != nil {
 		fmt.Println("File server start error: ", err)
+		fmt.Println("Reset port ...")
+
+		port++
+		return run()
 	}
 
+	return nil
 }
